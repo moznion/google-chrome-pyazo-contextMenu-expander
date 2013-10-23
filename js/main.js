a84fec181fc6da170a955c33eff8911927010a64
moznion <moznion@gmail.com>
2013-10-23 22:42:10 +0900

    Impl

--- js/main.js
+++ js/main.js
@@ -2,6 +2,36 @@ chrome.contextMenus.create({
     title: "Post image to Pyazo",
     contexts:["image"],
     onclick: function(info) {
-        alert(info.srcUrl);
+        pyazoPost(info.srcUrl);
     }
 });
+
+function pyazoPost(imageUrl) {
+    var pyazoUrl = "http://yairc.cfe.jp:5000/"
+    var preparePostImage = function () {
+        var dfd = $.Deferred();
+        $.ajax({
+            url: pyazoUrl,
+            type: "post",
+            contentType: "application/x-www-form-urlencoded",
+            datatype: "text",
+            data: {
+                "fileurl": imageUrl
+            },
+            crossDomain: true,
+            success: function (response) {
+                dfd.resolve(response);
+            },
+            error: function () {
+                dfd.reject();
+            }
+        });
+        return dfd.promise();
+    };
+
+    preparePostImage().done(function (response) {
+        alert(response);
+    }).fail(function () {
+        alert("ごめん、なんかしくじった");
+    });
+};
