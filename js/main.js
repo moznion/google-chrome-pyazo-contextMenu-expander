8a5a9d3ba5435027496bf04c455cecc0ed90609a
moznion <moznion@gmail.com>
2013-10-24 13:36:16 +0900

    Not be stationed.

--- js/main.js
+++ js/main.js
@@ -1,45 +1,73 @@
-chrome.contextMenus.create({
-    title: "Post image to Pyazo",
-    contexts:["image"],
-    onclick: function(info) {
-        pyazoPost(info.srcUrl);
-    }
-});
-
-function pyazoPost(imageUrl) {
-    var pyazoUrl = "http://yairc.cfe.jp:5000/"
-    var preparePostImage = function () {
-        var dfd = $.Deferred();
-        $.ajax({
-            url: pyazoUrl,
-            type: "post",
-            contentType: "application/x-www-form-urlencoded",
-            datatype: "text",
-            data: {
-                "fileurl": imageUrl
-            },
-            crossDomain: true,
-            success: function (response) {
-                dfd.resolve(response);
-            },
-            error: function () {
-                dfd.reject();
-            }
-        });
-        return dfd.promise();
-    };
-
-    preparePostImage().done(function (responseUrl) {
-        // url goes to clipboard
-        var copyArea = $("<textarea/>");
-        copyArea.text(responseUrl);
-        $("body").append(copyArea);
-        copyArea.select();
-        document.execCommand("copy");
-        copyArea.remove();
-
-        window.open(responseUrl); // show page
-    }).fail(function () {
-        alert("ごめん、なんかしくじった");
-    });
+var Background = function () {
+    this.initialize();
 };
+
+Background.prototype = {
+    initialize: function() {
+        this.assignEventHandlers();
+        this.createContextMenus();
+    },
+
+    assignEventHandlers: function () {
+        var that = this;
+        chrome.contextMenus.onClicked.addListener(function (info) {
+            that.onClickContextMenu(info);
+        });
+    },
+
+    createContextMenus: function () {
+        chrome.contextMenus.create({
+            id: "contextMenu_pyazo_post",
+            type: "normal",
+            title: "Post image to Pyazo",
+            contexts: ["image"]
+        });
+    },
+
+    onClickContextMenu: function (info) {
+        var id = info.menuItemId;
+        if (id === "contextMenu_pyazo_post") {
+            this.onClickPost(info);
+        }
+    },
+
+    onClickPost: function (info) {
+        var pyazoUrl = "http://yairc.cfe.jp:5000/";
+        var preparePostImage = function () {
+            var dfd = $.Deferred();
+            $.ajax({
+                url: pyazoUrl,
+                type: "post",
+                contentType: "application/x-www-form-urlencoded",
+                datatype: "text",
+                data: {
+                    "fileurl": info.srcUrl
+                },
+                crossDomain: true,
+                success: function (response) {
+                    dfd.resolve(response);
+                },
+                error: function () {
+                    dfd.reject();
+                }
+            });
+            return dfd.promise();
+        };
+
+        preparePostImage().done(function (responseUrl) {
+            // url goes to clipboard
+            var copyArea = $("<textarea/>");
+            copyArea.text(responseUrl);
+            $("body").append(copyArea);
+            copyArea.select();
+            document.execCommand("copy");
+            copyArea.remove();
+
+            window.open(responseUrl); // show page
+        }).fail(function () {
+            alert("Post Failed...");
+        });
+    }
+};
+
+var background = new Background();
